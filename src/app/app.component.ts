bd37db2a84e8dea58124ce409507e7e145971d78
yanioconjota <janioisacura@gmail.com>
2024-12-27 14:04:24 -0300

    Created custom material theme boilerplate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ThemeService } from './services/theme.service';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,11 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'pokedex-ng';
+  constructor(private themeService: ThemeService) {}
+
+  toggleTheme(): void {
+    const newTheme = this.themeService.currentTheme === 'light-theme' ? 'dark-theme' : 'light-theme';
+    this.themeService.setTheme(newTheme);
+    console.log(this.themeService.currentTheme);
+  }
 }
---
+++ src/app/services/theme.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ThemeService {
+
+  constructor() { }
+
+  private themeClass = localStorage.getItem('theme') ?? 'light-theme';
+
+  setTheme(theme: 'light-theme' | 'dark-theme') {
+    this.themeClass = theme;
+    localStorage.setItem('theme', theme);
+    document.documentElement.className = theme;
+  }
+
+  get currentTheme(): string {
+    return this.themeClass;
+  }
+}
